0e3f1b5e0105e8f2d3b2c23f69b66c4b10d1e793
china.Mr <china.ra0.cn@gmail.com>
2026-08-08 10:09:23 +0800

    [CMS] Switch to Payload for app/sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 import { services, companyInfo } from "@/lib/services";
-import { getArticles, getTags } from "@/lib/strapi";
+import { getArticles, getTags } from "@/lib/payload";
 
 export const revalidate = 3600;
 
